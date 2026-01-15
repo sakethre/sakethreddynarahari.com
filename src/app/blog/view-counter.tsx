ad14ca7f707381d073cc2b26969d0f400a72b191
Saketh Reddy <sakethreddynsr@gmail.com>
2026-01-15 10:15:16 -0800

    Fix Blog View Counter logic

--- src/app/blog/view-counter.tsx
+++ src/app/blog/view-counter.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect } from 'react'
 import useSWR from 'swr'
 
 import fetcher from '@/src/lib/fetcher'
@@ -11,10 +12,22 @@ export default function ViewCounter({
 	slug: string
 	trackView: boolean
 }) {
-	const { data } = useSWR<{ total: number }>(
+	const { data, mutate } = useSWR<{ total: number }>(
 		`/api/views/${slug}`,
 		fetcher
 	)
+
+	useEffect(() => {
+		if (!trackView) return
+
+		fetch(`/api/views/${slug}`, {
+			method: 'POST',
+		}).then((res) => {
+			if (res.ok) {
+				mutate()
+			}
+		})
+	}, [slug, trackView, mutate])
 
 	const views = new Number(data?.total || 0)
 
